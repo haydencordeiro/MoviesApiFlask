6ff06114ebe77b7d091dee262b3128b043c3c381
Hayden Cordeiro <40896224+haydencordeiro@users.noreply.github.com>
2020-10-17 15:54:25 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,48 @@
+from pymongo import MongoClient
+import flask
+import json
+from flask import request
+from flask import jsonify
+client = MongoClient(
+    "mongodb+srv://test:test@cluster0.vsegj.mongodb.net/<Cluster0>?retryWrites=true&w=majority")
+db = client.get_database('dbname')
+records = db.collname
+
+print(records.count_documents({}))
+
+
+def insertMovie(name, img, summary):
+    new_movie = {
+        'name': name,
+        'img': img,
+        'summary': summary
+    }
+
+    records.insert_one(new_movie)
+
+
+def showMovies():
+    return (list(records.find()))
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET', 'POST'])
+def getMovies():
+    if request.method == 'GET':
+        data = showMovies()
+        return json.dumps(data, default=str)
+    else:
+        try:
+            data = request.form.to_dict(flat=False)
+            insertMovie(data['name'][0], data['img'][0], data['summary'][0])
+            resp = jsonify(success=True)
+            return resp
+        except:
+            resp = jsonify(success=True)
+            return resp
+
+
+app.run()
